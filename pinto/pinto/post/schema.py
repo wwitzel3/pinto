0f0c345723f36635d850120a7d22eff1590cf14d
Wayne Witzel III <wayne@pieceofpy.com>
2013-08-03 20:24:42 -0400

    Added tag handling

--- pinto/pinto/post/schema.py
+++ pinto/pinto/post/schema.py
@@ -1,3 +1,4 @@
+from slugify import slugify
 import colander as c
 
 @c.deferred
@@ -12,8 +13,25 @@ def deferred_url_validator(node, kw):
             raise c.Invalid(node, u"A post with that url already exists.")
         return invalid_url
 
+class Tags(c.SchemaType):
+    """Uses a standard text input
+    Expects to see a comma seperated list of tags
+    """
+    def serialize(self, node, appstruct):
+        if appstruct in (c.null, None):
+            return ''
+        appstruct = ','.join(appstruct)
+        return appstruct
+
+    def deserialize(self, node, cstruct):
+        if not cstruct:
+            return c.null
+        cstruct = [slugify(t.strip()) for t in cstruct.split(',')]
+        return cstruct
+
+
 class Post(c.MappingSchema):
     url = c.SchemaNode(c.String(), validator=deferred_url_validator)
     title = c.SchemaNode(c.String())
     body = c.SchemaNode(c.String())
-
+    tags = c.SchemaNode(Tags())
